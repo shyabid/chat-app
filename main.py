c724d51bfc40609004fbcd87e77f48185dc6547b
shyabid <abidshahrear66@gmail.com>
2024-08-06 23:00:30 +0600

    authentication

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import pymongo
 app = Flask(__name__)
 app.secret_key = 'askd9001e'
 
-myclient = pymongo.MongoClient("mongodb://localhost:27017")
+myclient = pymongo.MongoClient("mongodb+srv://abid:1nQefcNAEIRYJ5gF@cluster0.jrkne.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 user_db = myclient["authentication"]
 user_table = user_db["user_info"]
 
@@ -29,7 +29,7 @@ def register_check():
         for x in query:
             if x["userid"] == req["userid"]:
                 flag = 1
-                break
+            break
         
         reg_dict = {
             "userid": req["userid"],
@@ -40,20 +40,45 @@ def register_check():
         if(flag == 0):
             temp = user_table.insert_one(reg_dict)
         else:
-            return "User already exists"
+            return render_template("invalid.html", message="User already exists")
 
-        return render_template('dashboard.html')
+        return render_template('dashboard.html', userid=req["userid"])
     return render_template('register.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template('login.html')
 
+
+@app.route('/login_check', methods=['GET', 'POST'])
+def login_check():
+    if(request.method == 'POST'):
+        req = request.form
+        req = dict(req)
+        print(req)
+        query = user_table.find({'userid': req["userid"]})
+        flag = 0
+        temp = None
+        for x in query:
+            if x["userid"] == req["userid"]:
+                flag = 1
+                temp = x
+                break
+        
+        if flag == 1:
+            if temp["password"] == req["password"]:
+                return render_template('dashboard.html', userid=req["userid"])
+            else:
+                return render_template("invalid.html", message="Incorrect password")
+        return render_template("invalid.html", message="User is not refistered")
+    
+        
+    return render_template("login.html")    
+        
 @app.route('/dashboard', methods=['GET', 'POST'])
 def ashboard():
     return render_template('dashboard.html')
 
+
 if __name__ == '__main__':
     app.run(debug=True, threaded=True)
-    
-    
